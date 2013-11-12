50bedb9dc1364e9c344a96e51d81b1badd220a48
Tony Ling <tl2573@columbia.edu>
2013-11-11 23:10:46 -0500

    updated tester and script

--- tests/reg_test.sh
+++ tests/reg_test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 STRING=""
+OUT=""
 FILENAME="./reg_test_results.txt"
-
+OUTFILENAME="./reg_test_results."
 #script runs tester on every slang file
 for FILE in *.sl;
 do
@@ -10,10 +11,15 @@ do
 	if [ "$RESULTS" ]; then
 		STRING+="$RESULTS\n"
 	else
-		STRING+="$FILE	NO\n"
+		STRING+="$FILE	exception\n"
 	fi
 done
 
 touch "$FILENAME"
 #printf "$STRING" >> "$FILENAME" for append instead of overwrite
 printf "$STRING" > "$FILENAME"
+
+#create single .out file from all output
+touch "$OUTFILENAME"
+cat *.out >> "$OUTFILENAME"
+
