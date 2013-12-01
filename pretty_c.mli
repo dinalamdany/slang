2794063b02ac73be604653aa92d9e34c9cc32f15
Tony Ling <tling@users.noreply.github.com>
2013-11-30 20:36:24 -0500

    Update pretty_c.mli

--- pretty_c.mli
+++ pretty_c.mli
@@ -2,20 +2,26 @@ open Ast
 
 type name =
     Init_name of string
-    | Always_name of string 
+    | Always_name of string
 
-type link = 
-    Link of name
+type link =
+    Link of string
+        
+type struct_obj=
+    Init_obj of name
+    | Always_obj of name * link 
 
 type main =
-    name list * name list
+    struct_obj list * struct_obj list * link list
 
 type structure =
     Init of name * int * func_decl
     | Always of name * int * link * func_decl
 
-type pretty_c = 
-    decl list * func_decl list * structure list * main
+type time_block =
+    decl list * structure list
 
-  
+ type pretty_c =
+    decl list * func_decl list * time_block list * main
+
 
