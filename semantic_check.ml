46b49faf921d6bdd509b2fcfb18947431d03e8c5
Livi Byer <okb2102@barnard.edu>
2013-12-05 21:26:39 -0500

    Adding symbol table functions

--- semantic_check.ml
+++ semantic_check.ml
@@ -4,39 +4,40 @@ open Type
 
 type symbol_table = {
 	parent: symbol_table option;
-	variables: (string * Type.var_type) list
+	variables: (string * Type.var_type * bool) list
 }
 
 type function_table = {
-	functions: (Type.var_type * string * Type.var_type list * stmt list) list
+	functions: (string * Type.var_type*Type.var_type list * stmt list) list
 }
 
 type translation_environment = {
 	return_type: var_type;	(*function's return type*)
 	return_seen: bool;		(*does the function have a return statement*)
+	location: string;		(*init, always, main, or function name*)
 	global_scope: symbol_table;	(*symbol table for global vairables*)
 	var_scope: symbol_table; 	(*symbol table for local variables*)
 	fun_scope: function_table;	(*symbol table for functions*)
 }
 
 (*function that adds variables to environment's var_scope for use in functions*)
-let add_to_var_table env name t = 
-	let new_vars = (s,t)::env.var_scope.variables in
+let add_to_var_table env name t is_array = 
+	let new_vars = (name,t,is_array)::env.var_scope.variables in
 	let new_sym_table = {parent = env.var_scope.parent; variables = new_vars} in
 	let new_env = {env with var_scope = new_sym_table} in
 	new_env
 
 (*function that adds variables to environment's global_scope for use with main*)
-let add_to_global_table env name t = 
-	let new_vars = (s,t)::env.global_scope.variables in
+let add_to_global_table env name t is_array= 
+	let new_vars = (name,t,is_array)::env.global_scope.variables in
 	let new_sym_table = {parent=env.global_scope.parent; variables = new_vars} in
 	let new_env = {env with global_scope = new_sym_table} in
 	new_env
 
 (*search for variable in global and local symbol tables*)
 let find_variable env name =
-	try List.find (fun (s,_,_,_) -> s=name) env.var_scope.variables
-	with Not_found -> try List.find(fun (s,_,_,_) -> s=name) env.global_scope.variables
+	try List.find (fun (s,_,_) -> s=name) env.global_scope.variables
+	with Not_found -> try List.find(fun (s,_,_) -> s=name) env.var_scope.variables
 	with Not_found -> raise Not_found
 
 (* search for a function in our function table*)
@@ -65,5 +66,18 @@ let check_assignments type1 typ2 = match (type1, type2) with
 (* add a function to the environment*)
 let add_function env func_declaration =
 	let f_table = env.fun_scope in
-	let old_functions = f_table.functions
-	let func_type=func_decl.  
+	let old_functions = f_table.functions in
+	let func_name=func_declaration.fname in
+	let func_type=func_declaration.return in
+	let func_formals=func_declaration.formals in
+	let func_body=func_declaration.body in
+	let new_functions = (func_name, func_type, func_formals, func_body)::old_functions in
+	let new_func_scope = {functions = new_functions} in
+	let new_env = {env with func_scope = new_fun_scope} in
+	new_env
+
+(* add a value to the symbol table*)
+let add_var env var_declaration is_array=
+	let sym_table= match env.location with
+		main -> add_to_global_table env var_declaration.ident var_declaration.datatype is_array
+		| _ -> add_to_local_table env var_declaration.ident var_declaration.datatype is_array
