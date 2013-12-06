12634ac302e75fb2050ab87ee18f975a68b1bf1a
Livi Byer <okb2102@barnard.edu>
2013-12-06 12:21:27 -0500

    Changing the sstmt in sast

--- sast.mli
+++ sast.mli
@@ -1,7 +1,7 @@
 open Ast
 open Type
 
-type sexpr =
+(*type sexpr =
 	IntLit of int
 	| BoolLit of bool
 	| StringList of string
@@ -14,9 +14,9 @@ type sexpr =
 	| Cast of datatype * sexpr
 	| Call of ident * sexpr list
 	| ObjProp of ident * ident
-
-type expr = 
-	Expr of sexpr * datatype
+*)
+type sexpr = 
+	Expr of expr * datatype
 
 type sdecl =
 	VarDecl of datatype * ident
@@ -24,18 +24,32 @@ type sdecl =
 	| ArrDecl of datatype * ident
 	| ArrAssignDecl of datatype * ident * sexpr list
 
-type decl = 
-	Decl of sdecl * datatype
-
-type sfunc_decl = {
+(*type sfunc_decl = {
 		return: datatype;
 		fname: ident;
 		formals: formal list;
 		body: stmt list;
 		}
+*)
+type sfunc_decl =
+	Func_Decl of func_decl * datatype
 
-type func_decl = 
-	Func_Decl of sfunc_decl * datatype
+type sstmt = 
+	SBlock of sstmt list
+	| SExpr of sexpr
+	| SReturn of sexpr
+	| SIf of sexpr * sstmt * sstmt
+	| SFor of sexpr * sexpr * sexpr * sstmt
+	| SWhile of sexpr * sstmt
+	| SDeclaration of sdecl
+	| SPropertyAssign of ident * ident * sexpr 
+	| SAssign of ident * sexpr
+	| SArrAssign of ident * sexpr list
+	| SArrElemAssign of ident * int * sexpr
+	| STerminate 
+
+(*type sstmt = 
+	Stmt of sstmt * datatype*)
 
 type sprogram = 
-	Prog of sfunc_decl list * (sdecl list * thread list) * Type.var_type
+	Prog of sfunc_decl list * (sdecl list * thread list) * datatype
