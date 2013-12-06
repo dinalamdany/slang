68a75a316db857d1f98ced3738773971acfc1a97
Tony Ling <tl2573@columbia.edu>
2013-12-06 12:01:29 -0500

    updated template

--- slang_template.cpp
+++ slang_template.cpp
@@ -55,6 +55,8 @@ struct event_q_ {
     void add(unsigned int time_, event_ *obj_) {
         bool eol = true;
         std::deque<event_*>::iterator it;
+        if (obj_ == NULL)
+            return;
         for (it = event_q.begin(); it != event_q.end(); it++) {
             if ((*it)->get_time() > time_) {
                 event_q.insert(it, obj_);
@@ -94,6 +96,7 @@ int u_find_ele_pos(int u_ele)
     return -1;
 }
 
+// *EDIT*  Read always blocks comments for updated init blocks
 // time blocks
 //   init blocks should just inherit event_
 //   Note: the prefixes for the init
@@ -103,21 +106,35 @@ int u_find_ele_pos(int u_ele)
 //  we need a variable declartion list stored in ocaml for init block code
 //  this example only has 1 declartion
 int init1_i;
+unsigned int init1_time = 0;
+struct init1_link_ : public event_ {
+    virtual void set_next(init1_link_ *n){};
+};
+std::vector<init1_link_*> init1_list;
 //  blocks contain all code between # delays and braces
 //  I used function foo as the name, but we could use any
 //  all code aside from variable declaration is found in each struct's foo
-struct init1_block1_ : public event_ {
+struct init1_block1_ : public init1_link_ {
     unsigned int time;
     init1_block1_() : time(0){}
     unsigned int get_time(){return time;};
+    init1_link_ *next;
+    void set_next(init1_link_ *n){next = n;}
     void foo(){
-        //nothing since just declares int i;
+        // nothing before #2 print(find_ele_pos(counter))
+        // so just puts next struct on list
+        // whenever an always foo() is called, it puts it's
+        // next object pointed to on the list
+        init1_time += next->get_time();
+        event_q.add(init1_time, next);
     }
 };
-struct init1_block2_ : public event_ {
+struct init1_block2_ : public init1_link_ {
     unsigned int time;
     init1_block2_() : time(1){}
     unsigned int get_time(){return time;};
+    init1_link_ *next;
+    void set_next(init1_link_ *n){next = n;}
     void foo(){
         // notice that we need to keep track of scope.
         // since init1_array_size and init1_array does not exist, then
@@ -127,15 +144,21 @@ struct init1_block2_ : public event_ {
         {
             u_array[init1_i] = u_array_size-init1_i;
         }
+        init1_time += next->get_time();
+        event_q.add(init1_time, next);
     }
 };
-struct init1_block3_ : public event_ {
+struct init1_block3_ : public init1_link_ {
     unsigned int time;
     init1_block3_() : time(25){}
     unsigned int get_time(){return time;};
+    init1_link_ *next;
+    void set_next(init1_link_ *n){next = n;}
     void foo(){
         // exit to Terminate, any objections?
         exit(0);
+        init1_time += next->get_time();
+        event_q.add(init1_time, next);
     }
 };
 
@@ -192,6 +215,7 @@ struct always1_block2_ : public always1_link_ {
 // the only differences would be the prefixes, ie init1_, init2_, always1_, always2_
 int main()
 {
+    /* OUTDATED
     // Init block object declartions for event q
     //  initiate init struct objects and add to event q afterwards.
     //  we could have a list of structs starting with "init" in ocaml
@@ -202,7 +226,23 @@ int main()
     event_q.add(init1_block2_obj.get_time(), &init1_block2_obj);
     init1_block3_ init1_block3_obj;
     event_q.add(init1_block3_obj.get_time(), &init1_block3_obj);
-
+    */
+    // Init blocks now act like always blocks
+    init1_block1_ init1_block1_obj;
+    init1_list.push_back(&init1_block1_obj);
+    init1_block2_ init1_block2_obj;
+    init1_list.push_back(&init1_block2_obj);
+    init1_block3_ init1_block3_obj;
+    init1_list.push_back(&init1_block3_obj);
+    // Same as always block, except if last block, don't go back to
+    for (int i = 0; i < init1_list.size(); i++)
+    {
+        if (i != init1_list.size()-1)
+            init1_list[i]->set_next(init1_list[i+1]);
+        else
+            init1_list[i]->set_next(NULL);
+    }
+    event_q.add(init1_block1_obj.get_time(), &init1_block1_obj);
     // Always block object declarations for event q
     //  create each always block, and for each always,
     //  we take the blocks associated with it and add it to a list
