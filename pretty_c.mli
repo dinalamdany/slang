52d6ea070286b6edc5b30b4126b2f881abbbb9ec
Tony Ling <tling@users.noreply.github.com>
2013-12-10 15:18:34 -0500

    all init/always struct objs in the same list

--- pretty_c.mli
+++ pretty_c.mli
@@ -9,8 +9,8 @@ type link =
 type struct_obj =
     Time_struct_obj of name * link
 
-type main = (*two link lists, init objects and always objects*)
-    struct_obj list * link list * struct_obj list * link list
+type main = (*two link lists, init objects and always objects stored in struct_obj list*)
+    struct_obj list * link list * link list
 
 type structure =
     Time_struct of name * int * link * stmt list
