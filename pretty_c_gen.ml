277f26b8a4bb7f016b00e5b4d397c1add2defd51
bitnami <mc3683@columbia.edu>
2013-12-12 18:26:07 -0500

    added support for mutable 'global' lists

--- pretty_c_gen.ml
+++ pretty_c_gen.ml
@@ -6,7 +6,7 @@ open Printf
 open Semantic_check
 let print = "print"
 
-(* Section for counters *)
+(* Counters *)
 
 let inc1 prefix =
    let count = ref (-1) in
@@ -14,9 +14,15 @@ let inc1 prefix =
      incr count;
      prefix ^ string_of_int !count
 
-let so_list = []
-let l1_list = []
-let l2_list = []
+(* Main_lists type *)
+type main_lists = {
+  mutable so: struct_obj list;
+  mutable l1: link list;
+  mutable l2: link list;
+}
+
+(* 'Global' variable for lists in main *)
+let m_lists = {so=[]; l1=[]; l2=[]};;
 
 (* 
 (* Counter Test *) 
@@ -45,7 +51,6 @@ let gen_decl = function
   SVarDecl(datatype, ident) -> VarDecl(datatype, ident)
 | SVarAssignDecl(datatype, ident, sval) -> VarAssignDecl(datatype, ident, (gen_val sval))
 
-(* TODO: What about SPropertyAssign? *)
 let rec gen_stmt  = function
 SBlock(sstmt_list) -> Block(List.map gen_stmt sstmt_list)
 | SSExpr(sexpr) -> Expr(gen_expr sexpr)
@@ -54,7 +59,6 @@ SBlock(sstmt_list) -> Block(List.map gen_stmt sstmt_list)
 | SFor(sexpr1, sexpr2, sexpr3, sstmt)-> For(gen_expr sexpr1, gen_expr sexpr2, gen_expr sexpr3, gen_stmt sstmt)
 | SWhile(sexpr1, sstmt) -> While(gen_expr sexpr1, gen_stmt sstmt)
 | SDeclaration(sdecl) -> Declaration(gen_decl sdecl)
-| SPropertyAssign(ident1, ident2, sexpr) -> Expr(gen_expr sexpr) (*Does this even exist anymore?*)
 | SAssign(ident, sexpr) -> Assign(ident, gen_expr sexpr)
 | SArrAssign(ident, sexpr_list) -> ArrAssign(ident, List.map gen_expr sexpr_list)
 | SArrElemAssign(ident, i, sexpr) -> ArrElemAssign(ident, i, gen_expr sexpr)
@@ -63,21 +67,21 @@ SBlock(sstmt_list) -> Block(List.map gen_stmt sstmt_list)
 (* Receives the link, creates a current name (i.e. init_1_block_1).
   Then creates the time struct with the name, the delay from event *)
 let gen_structure counter link = function
-  SEvent(delay, sstmt_list) -> let curr_name = inc1 (counter ^ "_block_") in
-                                let name = Time_struct_name(curr_name ()) in 
-                                 (* Time_struct_obj(name, counter) :: so_list; (*adding to main list*) *)
-                                 Time_struct(name, delay, link, List.map gen_stmt sstmt_list)
+  SEvent(delay, sstmt_list) -> let curr_name = inc1 (counter ^ "_block_") () in
+                                 let name = Time_struct_name(curr_name) in 
+                                   m_lists.so <- Time_struct_obj(name, link) :: m_lists.so; (*adding to main list*)
+                                   Time_struct(name, delay, link, List.map gen_stmt sstmt_list)
 
 (* Receives sthread, creates Link for either init or always,
   sends this link as parameter for gen_structure function *)
-(*  Todo: v_Decls for timeblocks are currently empty lists... Whats the actual value? *)
+(*  TODO: v_Decls for timeblocks are currently empty lists... Whats the actual value? *)
 let gen_time_block = function 
   SInit(sthread) -> let curr_count = inc1 "init_" () in
-                      let link = Link(curr_count) in (* l1_list := link :: l1_list; (*adding to main list*) *)
+                      let link = Link(curr_count) in m_lists.l1 <- link :: m_lists.l1; (*adding to main list*)
                         let partial_gen_struct = gen_structure curr_count link in
                           Time_block(link, [], List.map partial_gen_struct sthread)
 | SAlways(sthread) -> let curr_count = inc1 "always_" () in
-                        let link = Link(curr_count) in (* l2_list := link :: l2_list; (*adding to main list*) *)
+                        let link = Link(curr_count) in m_lists.l2 <- link :: m_lists.l2; (*adding to main list*)
                           let partial_gen_struct = gen_structure curr_count link in
                             Time_block(link, [], List.map partial_gen_struct sthread)                        
 
@@ -91,12 +95,6 @@ let gen_pretty_c prog = function
   Prog(sfunc_decl_list, (sdecl_list, sthread_list)) -> 
     let func_list = List.map gen_func sfunc_decl_list in (* Functions *)
     let decl_list = List.map gen_decl sdecl_list in (* Declarations *)
-    let time_block_list = List.map gen_time_block sthread_list in (* Time Blocks *)
-    let main = Main([], [], []) in (* Main *)
+    let time_block_list = List.map gen_time_block  sthread_list in (* Time Blocks *)
+    let main = Main(m_lists.so, m_lists.l1, m_lists.l2) in (* Main *)
       Pretty_c(decl_list, func_list, time_block_list, main)
-
-(* let _ = 
-  let lexbuf = Lexing.from_channel stdin in
-  let sabstract_syntax_tree = Semantic_check.check_program (Parser.program Scanner.token lexbuf) in
-  let pretty_c = gen_pretty_c sabstract_syntax_tree in
-    pretty_c *)
