83c5504a7b1ed729044b478152abee037133ae75
bitnami <mc3683@columbia.edu>
2013-12-12 16:36:47 -0500

    added constructors to prety_c

--- pretty_c.mli
+++ pretty_c.mli
@@ -10,14 +10,14 @@ type struct_obj =
     Time_struct_obj of name * link
 
 type main = (*two link lists, init objects and always objects stored in struct_obj list*)
-    struct_obj list * link list * link list
+    Main of struct_obj list * link list * link list
 
 type structure =
     Time_struct of name * int * link * stmt list
 
 type time_block =
-    link * decl list * structure list (*struct list counts the number of blocks *)
+    Time_block of link * decl list * structure list (*struct list counts the number of blocks *)
                                       (*name = link append block # *)
 
 type pretty_c =
-    decl list * func_decl list * time_block list * main
+    Pretty_c of decl list * func_decl list * time_block list * main
