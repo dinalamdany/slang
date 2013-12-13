ab93101ab75c23b2b76f9bfe66ff149349b7ccaf
bitnami <mc3683@columbia.edu>
2013-12-12 22:23:25 -0500

    fixed pretty_c_gen function to return correct type, fixed counters

--- pretty_c_gen.ml
+++ pretty_c_gen.ml
@@ -1,18 +1,18 @@
-open Type
-open Ast
-open Sast
-open Pretty_c
-open Printf
+open Ast 
+open Sast 
 open Semantic_check
-let print = "print"
+open Pretty_c
 
 (* Counters *)
-
 let inc1 prefix =
    let count = ref (-1) in
    fun () ->
      incr count;
      prefix ^ string_of_int !count
+
+(* Global counter functions *)
+let init_count = inc1 "init_"
+let always_count = inc1 "always_"
 
 (* Main_lists type *)
 type main_lists = {
@@ -21,22 +21,8 @@ type main_lists = {
   mutable l2: link list;
 }
 
-(* 'Global' variable for lists in main *)
+(* 'Global variable' for lists in main *)
 let m_lists = {so=[]; l1=[]; l2=[]};;
-
-(* 
-(* Counter Test *) 
-let init_count = inc1 "init_"
-let always_count = inc1 "always_"
-
-let () = print_endline (init_count ())  (* 0 *)
-let () = print_endline (init_count ())  (* 1 *)
-
-let () = print_endline (always_count ())  (* 0 *)
-let () = print_endline (init_count ())  (* 2 *)
-let () = print_endline (always_count ())  (* 1 *)
-
- *)
 
 (* ----------- Begin Pretty_c_gen functions --------------  *)
 
@@ -64,37 +50,49 @@ SBlock(sstmt_list) -> Block(List.map gen_stmt sstmt_list)
 | SArrElemAssign(ident, i, sexpr) -> ArrElemAssign(ident, i, gen_expr sexpr)
 | STerminate -> Terminate
 
-(* Receives the link, creates a current name (i.e. init_1_block_1).
+(* Receives the link, creates a current name (i.e. init_0_block_1).
   Then creates the time struct with the name, the delay from event *)
-let gen_structure counter link = function
-  SEvent(delay, sstmt_list) -> let curr_name = inc1 (counter ^ "_block_") () in
-                                 let name = Time_struct_name(curr_name) in 
-                                   m_lists.so <- Time_struct_obj(name, link) :: m_lists.so; (*adding to main list*)
-                                   Time_struct(name, delay, link, List.map gen_stmt sstmt_list)
+let gen_structure curr_name_f link = function
+  SEvent(delay, sstmt_list) -> let name = Time_struct_name(curr_name_f ()) in (* Counter *)
+                                 m_lists.so <- Time_struct_obj(name, link) :: m_lists.so; (*adding to main list*)
+                                 Time_struct(name, delay, link, List.map gen_stmt sstmt_list)
 
-(* Receives sthread, creates Link for either init or always,
+(* Receives sthread, creates Link for init or always,
   sends this link as parameter for gen_structure function *)
 (*  TODO: v_Decls for timeblocks are currently empty lists... Whats the actual value? *)
 let gen_time_block = function 
-  SInit(sthread) -> let curr_count = inc1 "init_" () in
-                      let link = Link(curr_count) in m_lists.l1 <- link :: m_lists.l1; (*adding to main list*)
-                        let partial_gen_struct = gen_structure curr_count link in
-                          Time_block(link, [], List.map partial_gen_struct sthread)
-| SAlways(sthread) -> let curr_count = inc1 "always_" () in
-                        let link = Link(curr_count) in m_lists.l2 <- link :: m_lists.l2; (*adding to main list*)
-                          let partial_gen_struct = gen_structure curr_count link in
-                            Time_block(link, [], List.map partial_gen_struct sthread)                        
+  SInit(sthread) ->
+    let curr_link_str = init_count () in let curr_name_f = inc1 (curr_link_str ^ "_block_") in (*Counters*)
+      let link = Link(curr_link_str) in m_lists.l1 <- link :: m_lists.l1; (*adding to main list*)
+        let partial_gen_struct = gen_structure curr_name_f link in
+          Time_block(link, [], List.map partial_gen_struct sthread)
+| SAlways(sthread) -> 
+  let curr_link_str = always_count () in let curr_name_f = inc1 (curr_link_str ^ "_block_") in (*Counters*)
+    let link = Link(curr_link_str) in m_lists.l2 <- link :: m_lists.l2; (*adding to main list*)
+      let partial_gen_struct = gen_structure curr_name_f link in
+        Time_block(link, [], List.map partial_gen_struct sthread)                        
 
 let gen_func = function
   Func_Decl(func_decl, datatype) -> func_decl
 
-(* TODO: lists in main are not being populated. The lines where the lists have to be populated are
-   commented in the gen_time_block, and gen_structure functions *)
+(*TODO: weird extra empty timeblock getting generated. Is it here?*)
 (* Main function to generate a pretty_c *)
-let gen_pretty_c prog = function 
+let gen_pretty_c = function 
   Prog(sfunc_decl_list, (sdecl_list, sthread_list)) -> 
     let func_list = List.map gen_func sfunc_decl_list in (* Functions *)
     let decl_list = List.map gen_decl sdecl_list in (* Declarations *)
     let time_block_list = List.map gen_time_block  sthread_list in (* Time Blocks *)
     let main = Main(m_lists.so, m_lists.l1, m_lists.l2) in (* Main *)
-      Pretty_c(decl_list, func_list, time_block_list, main)
+      Pretty_c(decl_list, func_list, time_block_list, main) (* Pretty_c *)
+
+(* 
+(* Counter Test *) 
+
+let () = print_endline (init_count ())  (* 0 *)
+let () = print_endline (init_count ())  (* 1 *)
+
+let () = print_endline (always_count ())  (* 0 *)
+let () = print_endline (init_count ())  (* 2 *)
+let () = print_endline (always_count ())  (* 1 *)
+
+ *)
