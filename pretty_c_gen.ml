e46737d1705b595fa0e4471aa936d79d8b8c95e7
bitnami <mc3683@columbia.edu>
2013-12-13 21:25:50 -0500

    added v_decl within timeblock support

--- pretty_c_gen.ml
+++ pretty_c_gen.ml
@@ -20,16 +20,18 @@ type main_lists = {
   mutable so: struct_obj list;
   mutable l1: link list;
   mutable l2: link list;
+  mutable v_decls: decl list;
 }
 
 (* 'Global variable' for lists in main *)
-let m_lists = {so=[]; l1=[]; l2=[]};;
+let m_lists = {so=[]; l1=[]; l2=[]; v_decls=[]};;
 
 (* ----------- Begin Pretty_c_gen functions --------------  *)
 
+(* Statement declaration *)
+
 let gen_ident = function
   SIdent(ident,scope) -> ident
-
 
 let rec gen_expr = function
   SIntLit(i,datatype) -> IntLit(i)
@@ -65,6 +67,43 @@ SBlock(sstmt_list) -> Block(List.map gen_stmt sstmt_list)
 | SArrElemAssign(sident, i, sexpr) -> ArrElemAssign(gen_ident sident, i, gen_expr sexpr)
 | STerminate -> Terminate
 
+(* Section for special v_decl filtering *)
+
+let rec gen_tb_expr = function
+  SIntLit(i,datatype) -> IntLit(i)
+| SBoolLit(b, datatype) -> BoolLit(b)
+| SFloatLit(f,datatype) -> FloatLit(f)
+| SStringLit(s,datatype) -> StringLit(s)
+| SVariable(sident, datatype) -> Variable(gen_ident sident)
+| SUnop(unop, sexpr, datatype)-> Unop(unop, gen_expr sexpr)
+| SBinop(sexpr, binop, sexpr2, datatype) -> Binop(gen_tb_expr sexpr, binop, gen_tb_expr sexpr2)
+| SArrElem(sident, i, datatype) -> ArrElem(gen_ident sident, i)
+| SExprAssign(sident, sexpr, datatype) -> ExprAssign(gen_ident sident, gen_tb_expr sexpr)
+| SCast(datatype, sexpr, datatype2) -> Cast(datatype, gen_tb_expr sexpr)
+| SCall(sident, sexpr_list, datatype) -> Call(gen_ident sident, List.map gen_tb_expr sexpr_list)
+
+let gen_tb_decl = function
+  SVarDecl(datatype, sident) -> m_lists.v_decls <- VarDecl(datatype, gen_ident sident) :: m_lists.v_decls;
+                                VarDecl(datatype, gen_ident sident)
+| SVarAssignDecl(datatype, sident, sval) -> m_lists.v_decls <- VarDecl(datatype, gen_ident sident) :: m_lists.v_decls;
+                                            VarAssignDecl(datatype, gen_ident sident, (gen_val sval))
+
+let rec gen_tb_stmt  = function
+SBlock(sstmt_list) -> Block(List.map gen_tb_stmt sstmt_list)
+| SSExpr(sexpr) -> Expr(gen_tb_expr sexpr)
+| SReturn(sexpr) -> Return(gen_tb_expr sexpr)
+| SIf(sexpr ,sstmt1, sstmt2)-> If(gen_tb_expr sexpr, gen_tb_stmt sstmt1, gen_tb_stmt sstmt2)
+| SFor(sexpr1, sexpr2, sexpr3, sstmt)-> For(gen_tb_expr sexpr1, gen_tb_expr sexpr2, gen_tb_expr sexpr3, gen_tb_stmt sstmt)
+| SWhile(sexpr1, sstmt) -> While(gen_tb_expr sexpr1, gen_tb_stmt sstmt)
+| SDeclaration(sdecl) -> Declaration(gen_tb_decl sdecl)
+| SAssign(sident, sexpr) -> Assign(gen_ident sident, gen_tb_expr sexpr)
+| SArrAssign(sident, sexpr_list) -> ArrAssign(gen_ident sident, List.map gen_tb_expr sexpr_list)
+| SArrElemAssign(sident, i, sexpr) -> ArrElemAssign(gen_ident sident, i, gen_tb_expr sexpr)
+| STerminate -> Terminate
+
+let gen_tb_vdecls = function
+  SEvent(delay, sstmt_list) -> List.map gen_tb_stmt sstmt_list
+
 (* Receives the link, creates a current name (i.e. init_0_block_1).
   Then creates the time struct with the name, the delay from event *)
 let gen_structure curr_name_f link = function
@@ -76,25 +115,27 @@ let gen_structure curr_name_f link = function
   sends this link as parameter for gen_structure function *)
 (*  TODO: v_Decls for timeblocks are currently empty lists... Whats the actual value? *)
 let gen_time_block = function 
-  SInit(sthread) ->
+  SInit(sthread) -> m_lists.v_decls <- []; (* Reset v_decl list *)
     let curr_link_str = init_count () in let curr_name_f = inc1 (curr_link_str ^ "_block_") in (*Counters*)
       let link = Link(curr_link_str) in m_lists.l1 <- link :: m_lists.l1; (*adding to main list*)
         let partial_gen_struct = gen_structure curr_name_f link in
-          Time_block(link, [], List.map partial_gen_struct (List.rev sthread))
-| SAlways(sthread) -> 
+          List.map gen_tb_vdecls sthread;
+          Time_block(link, m_lists.v_decls, List.map partial_gen_struct (List.rev sthread))
+| SAlways(sthread) -> m_lists.v_decls <- [];
   let curr_link_str = always_count () in let curr_name_f = inc1 (curr_link_str ^ "_block_") in (*Counters*)
     let link = Link(curr_link_str) in m_lists.l2 <- link :: m_lists.l2; (*adding to main list*)
       let partial_gen_struct = gen_structure curr_name_f link in
-        Time_block(link, [], List.map partial_gen_struct (List.rev sthread))                     
+        List.map gen_tb_vdecls sthread;
+        Time_block(link, m_lists.v_decls, List.map partial_gen_struct (List.rev sthread))                     
 
-(* let gen_func = function
-  SFunc_Decl(sfuncstr, datatype) -> {return=sfuncstr.sreturn; sfname=sfuncstr.sname; 
-                                    formals=sfuncstr.sname; body=sfuncstr.sbody} *)
+let gen_func = function
+  SFunc_Decl(sfuncstr, datatype) -> {return=sfuncstr.sreturn; fname=sfuncstr.sfname; 
+                                    formals=sfuncstr.sformals; body= List.map gen_stmt sfuncstr.sbody}
 
 (* Main function to generate a pretty_c *)
 let gen_pretty_c = function 
   Prog(sfunc_decl_list, (sdecl_list, sthread_list)) -> 
-    let func_list = [] in (* List.map gen_func sfunc_decl_list in (* Functions *) *)
+    let func_list = List.map gen_func sfunc_decl_list in (* Functions *)
     let decl_list = List.map gen_decl sdecl_list in (* Declarations *)
     let time_block_list = List.map gen_time_block (List.rev sthread_list) in (* Time Blocks *)
     let main = Main(m_lists.so, m_lists.l1, m_lists.l2) in (* Main *)
