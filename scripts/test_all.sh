be29506e00114f9c2fd7cd2cacecdcdc5625dde7
Tony Ling <tl2573@columbia.edu>
2013-12-14 15:07:59 -0500

    test_all.sh changed for reg tests

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
-#script that calls all other scripts
+#script used for reg testing
 COMPILER="../compiler_v3"
-"g++ output.cpp"
+COMPFILE="temp_test"
+
 for TESTFILE in ../tests/*.sl;
 do
-	echo "$TESTFILE"
+	echo "testing $TESTFILE"
+	OUTFILENAME="${TESTFILE:0:-3}.output"
+	TESTFILENAME="${TESTFILE:0:-3}.out"
 	"$COMPILER" < "$TESTFILE"
-
+	g++ output.cpp -o "$COMPFILE"
+	./"$COMPFILE" > "$OUTFILENAME"
+	RESULTS=`./"$COMPFILE"`
+	printf "$RESULTS" > "$OUTFILENAME"
+	if (diff "$OUTFILENAME" "$TESTFILENAME") 
+	then
+		echo "$OUTFILENAME matches $TESTFILENAME"
+	else
+		echo "$OUTFILENAME does not match $TESTFILENAME"
+	fi
+	rm "$OUTFILENAME" output.cpp "$COMPFILE"
 done
+
+#COMPILER="../compiler_v2"
+#COMPFILE="helloworld"
+#FILENAME=".././tests/helloworld.sl"
+#OUTFILENAME="helloworld.output"
+#TESTFILENAME=".././tests/helloworld.out"
+
+#"$COMPILER" < "$FILENAME"
+#g++ output.cpp -o "$COMPFILE"
+#./"$COMPFILE" > "$OUTFILENAME"
+#RESULTS=`./"$COMPFILE"`
+#RESULTS+="\n"
+#printf "$RESULTS" > "$OUTFILENAME"
+#if (diff "$OUTFILENAME" "$TESTFILENAME") 
+#then
+#	echo "$OUTFILENAME matches $TESTFILENAME"
+#else
+#	echo "$OUTFILENAME does not match $TESTFILENAME"
+#fi
+#
+#rm "$OUTFILENAME" output.cpp "$COMPFILE"
